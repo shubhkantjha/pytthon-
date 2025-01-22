10200838fc4b62b1cc466d820572a0285e2c41ad
shubhkantjha <shubhkantjha16@gmail.com>
2025-01-22 19:34:10 +0530

    Add files via upload

---
+++ lab2 Q10 python.py
@@ -0,0 +1,15 @@
+a=int(input('enter the first number'))
+b=int(input('enter the second number'))
+print("1 for addition") 
+print("2 for subtraction")
+print("3 for multiplication")
+print("4 for division")
+c=int(input())
+if(c==1):
+    print(a+b)
+elif(c==2):
+    print(a-b)
+elif(c==3):
+    print(a*b)
+elif(c==4):
+    print(a/b)    
