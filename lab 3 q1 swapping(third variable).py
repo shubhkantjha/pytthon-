ca7e0436b9ed1cb0629f2f07ecdbc43ac761bee2
shubhkantjha <shubhkantjha16@gmail.com>
2025-01-28 10:07:44 +0530

    Add files via upload

---
+++ lab 3 q1 swapping(third variable).py
@@ -0,0 +1,9 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+#swapping of numbers by third variable
+temp3=num1
+num1=num2
+num2=temp3
+print("swapped")
+print("first number :",num1)
+print("second number:",num2)
