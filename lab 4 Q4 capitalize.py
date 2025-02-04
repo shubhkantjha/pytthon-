c8ccdade2690a9f2f518bafd0e1d82fad501949a
shubhkantjha <shubhkantjha16@gmail.com>
2025-02-04 10:57:23 +0530

    Update lab 4 Q4 capitalize.py

--- lab 4 Q4 capitalize.py
+++ lab 4 Q4 capitalize.py
@@ -1,2 +1,11 @@
-s=input("enter name: ")
-print(s[0].upper()+s[1:-1] +s[-1].upper())
+string = input("Enter a string: ")
+words = string.split()  # Split the string into words
+result = []
+
+for word in words:
+    if len(word) == 1:
+        result.append(word.upper())  # Capitalize single-letter words
+    else:
+        result.append(word[0].upper() + word[1:-1] + word[-1].upper())  # Capitalize first and last letter
+
+print("Transformed string:", ' '.join(result))
