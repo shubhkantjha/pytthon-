d87bebdb3604f0d93f0f555540428826359f3270
Shashikant jha <shubhkantjha16@gmail.com>
2025-02-24 22:34:17 +0530

    Add files via upload

---
+++ lab 8 Q4.py
@@ -0,0 +1,8 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+# Check for common elements
+common_elements = set1 & set2
+if common_elements:
+    print("Common elements:", common_elements)
+else:
+    print("No common elements.")
