718bfd5d9d8ba09f8b9e7a15664591687d96de7e
Shashikant jha <shubhkantjha16@gmail.com>
2025-02-24 22:29:02 +0530

    Add files via upload

---
+++ lab 8 Q1.py
@@ -0,0 +1,6 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# Identical items (Intersection)
+identical_items = set1 & set2  # or set1.intersection(set2)
+print("Identical Items:", identical_items)
