a64690d1099fabcccfa4bd42696adbb0224e0271
Shashikant jha <shubhkantjha16@gmail.com>
2025-02-24 22:13:11 +0530

    Add files via upload

---
+++ lab 7 Q7.py
@@ -0,0 +1,5 @@
+my_tuple = (10, 20, 30, 40, 50, 60)
+fourth_element = my_tuple[3]
+fourth_from_last = my_tuple[-4]
+print("4th Element:", fourth_element)
+print("4th Element from last:", fourth_from_last)
